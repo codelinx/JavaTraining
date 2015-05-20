92ac7f6c9a3e4f4eed960e85ce990ab3f086d272
codelinx <codelinx@gmail.com>
2015-05-20 01:00:10 -0400

    Change-Id: Iec22df1ea6537253fb41cd66973b83f2dd3fa63b

--- src/com/java/training/Arrays.java
+++ src/com/java/training/Arrays.java
@@ -7,14 +7,16 @@ public class Arrays {
 	private static int rand = (int) ((Math.random() * 10));
 	private static int arraySize = rand;
 
-	public static void Main() {
+	public void Main() {
 		System.out.println("Main start ...");
 		createArrays();
 		getValue();
 		printArray();
+		bubbleSort2For();
+		// bubbleSortWhile();
 	}
 
-	public static void printArray() {
+	private static void printArray() {
 		System.out.println("|-------|-------|");
 		System.out.println("| Index | Value |");
 		for (int i = 0; i < arraySize; i++) {
@@ -39,7 +41,7 @@ public class Arrays {
 		System.out.println("|-------|-------|");
 	}
 
-	public static void createArrays() {
+	private static void createArrays() {
 		System.out.println("Output Random Number:" + rand);
 		for (int i = 0; i < arraySize; i++) {
 			int localRand = (int) ((Math.random() * 10));
@@ -50,7 +52,7 @@ public class Arrays {
 		// printArray();
 	}
 
-	public static int searchValueAtIndex(int searchIndex) {
+	private static int searchValueAtIndex(int searchIndex) {
 		if (searchIndex < arraySize) {
 			System.out.println("The value in index [" + searchIndex + "] is: "
 					+ normalArray[searchIndex]);
@@ -60,7 +62,7 @@ public class Arrays {
 		return 0;
 	}
 
-	public static int getValue() {
+	private static int getValue() {
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		// String checkDecision = null;
@@ -69,7 +71,7 @@ public class Arrays {
 		int value = input.nextInt();
 		searchValueAtIndex(value);
 		searchValueInArray(value);
-//		BubbleSortArray(value);
+		// BubbleSortArray(value);
 
 		// if (value <= arraySize && isFalse) {
 		// System.out.println("Is '" + value
@@ -102,7 +104,7 @@ public class Arrays {
 		return 0;
 	}
 
-	public static void searchValueInArray(int searchValue) {
+	private static void searchValueInArray(int searchValue) {
 		for (int i = 0; i < arraySize; i++) {
 			if (searchValue == normalArray[i]) {
 				System.out.print("Binary search " + normalArray[i] + ". ");
@@ -115,7 +117,7 @@ public class Arrays {
 		}
 	}
 
-	// public static int[] BubbleSortArray(int searchValue){
+	// private static int[] BubbleSortArray(int searchValue){
 	// int passedValue = 0;
 	// for (int i = 0; i < arraySize; i++) {
 	// if (normalArray[i] < normalArray[i+1]){
@@ -128,7 +130,7 @@ public class Arrays {
 	// return normalArray;
 	// }
 
-	private void bubbleSortWhile() {
+	private static void bubbleSortWhile() {
 
 		System.out.println("|-------|-------|");
 		System.out.println("|--Bubble Sort--|");
@@ -167,25 +169,35 @@ public class Arrays {
 		printArray();
 	}
 
-	private void bubbleSort2For() {
+	private static void bubbleSort2For() {
 
 		System.out.println("|-------|-------|");
 		System.out.println("|--Bubble Sort--|");
 		System.out.println("|-------|-------|");
+		System.out.println("|-Array Size: " + arraySize + "-|");
+		System.out.println("|-------|-------|");
 
 		int temp;
-		boolean isFalse = true;
-
+		// @SuppressWarnings("unused")
+		// boolean isFalse = true;
 		for (int i = 0; i < arraySize; i++) {
-			for (int j = 0; j < arraySize; j++) {
-				if (normalArray[i] > normalArray[i + 1]) {
-					temp = normalArray[i];
-					normalArray[i] = normalArray[i + 1];
-					normalArray[i + 1] = temp;
-					isFalse = true;
+			// isFalse = false;
+			for (int j = arraySize; arraySize > j; j--) {
+				// Logic for descending order Bubble sort
+				// if (normalArray[j] < normalArray[j + 1]) {
+				// temp = normalArray[j];
+				// normalArray[j] = normalArray[j + 1];
+				// normalArray[j + 1] = temp;
+				// isFalse = true;
+				if (normalArray[i] < normalArray[i+1]) {
+					temp = normalArray[j-1];
+					normalArray[j-1] = normalArray[j];
+					normalArray[j] = temp;
+					printArray();
 				}
 			}
 		}
+		System.out.println("|-----Final-----|");
 		printArray();
 	}
 
